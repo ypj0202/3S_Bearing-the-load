dae37b2a3a04f968554b50017ac5bb37754d12a0
anhhong225 <anhhong225@gmail.com>
2021-11-26 14:56:01 +0100

    Update dataCollection.py

--- dataCollection.py
+++ dataCollection.py
@@ -5,25 +5,29 @@ from _typeshed import WriteableBuffer
 import serial
 import csv
 import time
+import schedule
 
-arduino_port = "COM3"
-baud = 115200
-fileName = "data.csv"
+def func():
+    arduino_port = "COM3"
+    baud = 115200
+    fileName = "data.csv"
 
-start_time = time.time()
+    ser = serial.Serial(arduino_port, baud)
+    ser.flushInput()
 
-ser = serial.Serial(arduino_port, baud)
-ser.flushInput()
+    while True:
+        ser_data = ser.readline().decode().strip().split(',')
+    
+        #convert to int
+        new_ser_data = [int(i) for i in ser_data]
+    
+        with open(fileName, "a", newline='') as f:
+            write = csv.writer(f, delimiter = ",")
+            write.writerow([new_ser_data[0], new_ser_data[1], new_ser_data[2]])
+            f.close()
+
+schedule.every(10).second.do(func)
 
 while True:
-    ser_data = ser.readline().decode().strip().split(',')
-    
-    #convert to int
-    new_ser_data = [int(i) for i in ser_data]
-    
-    with open(fileName, "a", newline='') as f:
-        write = csv.writer(f, delimiter = ",")
-        write.writerow([new_ser_data[0], new_ser_data[1], new_ser_data[2]])
-        f.close()
-
+    schedule.run_pending()
 
