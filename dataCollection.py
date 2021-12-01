10fca030a4a9cdded0ceaab1822f242618b1b6c5
anhhong225 <anhhong225@gmail.com>
2021-12-01 12:09:38 +0100

    Update dataCollection.py

--- dataCollection.py
+++ dataCollection.py
@@ -6,16 +6,19 @@ import serial
 import csv
 import datetime
 
-
+#Already test with Uno
+#function to count the total row in the csv file
 def countRow(filename):
     file = open(filename, 'r')
     reader = csv.reader(file)
     lines = len(list(reader))
     return int(lines)
 
-
+#connect to arduino
+#collect output from arduino
+#write data into the csv file
 def readData(filename, port, baud, hour, minute, second):
-    arduino = serial.Serial(port, baud)
+    arduino = serial.Serial(port, baud, timeout = 0.01)
     runTime = time.time() + hour*3600 +minute*60 +second
     try:
         file = open(filename)
@@ -23,15 +26,21 @@ def readData(filename, port, baud, hour, minute, second):
         file = open(filename, "a+", newline='')
         csv_writer = csv.writer(file)
     while True:
-        if round(runTime - time.time()) == 0:
+        if round(runTime - time.time()) == 0: 
             break
-        data = arduino.readline()[:-2].decode()
+        data = arduino.readline()#[:-2].decode()
+        #datararray = data.decode().split(',')
+        #temp = float(dataarray[0])
+        #acc1 = int(dataarray[1])
+        #mic1 = int(dataarray[3])
         data = data.split(',')  # use coma to seperate the values
         if data:
         #if data & len(data) == 6:
+            #csv_writer.writerow([temp, acc1, mic1])
             csv_writer.writerow(data)
             print(data)
             file.flush()
+            file.close()
 
 
 now = datetime.datetime.now()
@@ -39,6 +48,6 @@ date_string = now.strftime("%d_%m_%Y_%H_%M_%S")
 csv_file = "sensor_output_" + date_string + '.csv'
 readData(csv_file, 'COM3', 115200, 0, 0, 5)
 
-
+#count the total row from csv file at the end
 rows = countRow(csv_file)
 print("Total values in file:", rows)
