93514be16e88787330e62281d3e778ce891a6552
Ken Yeh <ypj.0202@gmail.com>
2021-12-01 12:05:22 +0100

    Update TODO on Sensor code

--- 3S_Bearing_the_load/3S_Bearing_the_load.ino
+++ 3S_Bearing_the_load/3S_Bearing_the_load.ino
@@ -2,34 +2,48 @@
 * 3S_Bearing_the_load.ino
 * Description: A program running on Arduino DUE measuring acceleration, temperature and noise
 * Author: Ken Yeh 475496
-* Date: 25-11-2021
-* Revision: 1.0 
+* Date: 01-12-2021
+* Revision: 1.01 
 */
+#define A0 7
+#define A1 6
+#define A2 5
+#define A3 4
+#define A4 3
+#define A5 2
 int temperature_raw, accelerometer, microphone;
-float R1 = 10000;
-float logR2, R2, temp, temp_C;
-float c1 = 1.129252142e-03, c2 = 2.341083183e-04, c3 = 0.8773267909e-07;
+// float R1 = 10000;
+// float logR2, R2, temp, temp_C;
+// float c1 = 1.129252142e-03, c2 = 2.341083183e-04, c3 = 0.8773267909e-07;
 boolean conversionCompleted;
 //Configuration
-int sample_frequency = 3000;
-int prescaler_value = 32;
-int TC_RC_val = 84000000 / prescaler_value / sample_frequency;
+int sample_frequency = 20000;
+int prescaler_value = 2;
+int TC_RC_val = 84000000 / prescaler_value / sample_frequency; //Warning!!!!
+uint counter = 0;
 
 void setup()
 {
   // open a serial connection
-  Serial.begin(115200);
+  // Serial.begin(115200);
+  // Serial.println("Test");
+  SerialUSB.begin(0);
   // Timer config
   // Internel clock selection MCK/2 MCK/8 MCK/32 MCK/128
+// TIMER_CLOCK1 Clock selected: internal MCK/2 clock signal (from PMC)
+// TIMER_CLOCK2 Clock selected: internal MCK/8 clock signal (from PMC)
+// TIMER_CLOCK3 Clock selected: internal MCK/32 clock signal (from PMC)
+// TIMER_CLOCK4 Clock selected: internal MCK/128 clock signal (from PMC)
+// TIMER_CLOCK5 Clock selected: internal SLCK clock signal (from PMC)
   PMC->PMC_PCER0 |= PMC_PCER0_PID29;                     // TC2 power ON : Timer Counter 0 channel 2 is TC2
-  TC0->TC_CHANNEL[2].TC_CMR = TC_CMR_TCCLKS_TIMER_CLOCK3 // MCK/32, clk on rising edge
+  TC0->TC_CHANNEL[2].TC_CMR = TC_CMR_TCCLKS_TIMER_CLOCK1 // MCK/2, clk on rising edge
                               | TC_CMR_WAVE              // Waveform mode
                               | TC_CMR_WAVSEL_UP_RC      // UP mode with automatic trigger on RC Compare
                               | TC_CMR_ACPA_CLEAR        // Clear TIOA2 on RA compare match
                               | TC_CMR_ACPC_SET;         // Set TIOA2 on RC compare match
-
+  //TODO
   TC0->TC_CHANNEL[2].TC_RC = TC_RC_val;     //Sample frequency(Hz) = (Mck/prescaler_value)/TC_RC
-  TC0->TC_CHANNEL[2].TC_RA = TC_RC_val / 2; //Any Duty cycle
+  TC0->TC_CHANNEL[2].TC_RA = 2000; //Any Duty cycle DO MANUALLY
 
   TC0->TC_CHANNEL[2].TC_CCR |= TC_CCR_SWTRG | TC_CCR_CLKEN; // Software trigger TC2 counter and enable
 
@@ -54,7 +68,8 @@ void ADC_Handler()
   accelerometer = ADC->ADC_CDR[7]; 
   //Temperature sensor
   //DataSheet https://www.farnell.com/datasheets/1756131.pdf
-  temperature_raw = ADC->ADC_CDR[6]; 
+  // temperature_raw = ADC->ADC_CDR[6]; 
+  temperature_raw = ADC->ADC_CDR[A1];
   //Microphone
   microphone = ADC->ADC_CDR[5]; 
   conversionCompleted = true;
@@ -64,17 +79,16 @@ void loop()
 {
   if (conversionCompleted)
   {
-    //Steinhart–Hart equation Calculator https://www.thinksrs.com/downloads/programs/therm%20calc/ntccalibrator/ntccalculator.html
-    R2 = R1 * (1023.0 / (float)temperature_raw - 1.0);
-    logR2 = log(R2);
-    temp = (1.0 / (c1 + c2 * logR2 + c3 * logR2 * logR2 * logR2));
-    temp_C = temp - 273.15;
-    //Test Output
-    //Serial.println(accelerometer);
-    //Serial.println(microphone);
-    //Serial.print(temp_C);
-    //CSV Output
-    Serial.println(String(temp_C) + "," + String(microphone) + "," + String(accelerometer));
+    // char buffer[30];
+    // sprintf(buffer, "%d", temperature_raw);
+   
+
+    // TODO Convert int to char array and send via serial.print by not using String type since it is time consuming!
+
+    // SerialUSB.print(String(temperature_raw) + ',' + String(microphone) + ',' + String(accelerometer) + ',' + String(counter) + '\n');
+    // SerialUSB.print(buffer);
+    SerialUSB.print("456,1023,1023\n");
     conversionCompleted = false;
+    counter++;
   }
 }
